5d81caf83cd660e34e6fb6588468a6ac4db9b831
AnnaCape <61944693+AnnaCape@users.noreply.github.com>
2020-08-13 13:40:39 -0300

    pb processos corigido

--- src/main.c
+++ src/main.c
@@ -7,104 +7,78 @@
  * usando 4 processor maximo
  *Os numeros tem tamanho macimo de 2^64 <=> long long int 
  */
- 
-#include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <stdlib.h> 
+#include <sys/types.h> 
 #include <unistd.h>
-#include <signal.h>
 #include <sys/mman.h>
+#include <sys/wait.h>
 
 #define N_PROCESS_MAX 4
 
-//saber se eh primo
-bool eh_primo(int x){
-	bool eh= true;
-	// 0 e 1 não são primos
-	if(x < 2) eh =false;
-	
-	for (int i = 2; i < x; i++){
-		if (x % i == 0){
-      eh=false;
-		}
-	}
-	return eh;
-}
-
-// main program
 int main() {
-
-	// entrada
+  
+  	// entrada
 	long long int entrada[100];  
 	char c; 
-
-	// Definir flags de protecao e visibilidade de memoria
-	int protection = PROT_READ | PROT_WRITE;
-	int visibility = MAP_SHARED | MAP_ANON;
-
-	// Criar area de memoria compartilhada
-	int *cuantos_primos = (int *) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
-
-	// int na entrada
 	int i = 0;
+	// int na entrada
 	while(c != '\n'){
-		scanf("%llu", &entrada[i]);
+		scanf("%lld", &entrada[i]);
 		c = getchar();
 		i++;
 	}
+  int entrada_size = i;
+  
+  // Definir flags de protecao e visibilidade de memoria
+  int protection = PROT_READ | PROT_WRITE;
+  int visibility = MAP_SHARED | MAP_ANON;
 
-	int entrada_size = i;
+  // Criar area de memoria compartilhada
+  int *cuantos_primos, *p_ativos;
+  cuantos_primos = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
+  p_ativos = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
+  //p_ativos = quantos processos estão funcionando atualmente
+  (*p_ativos) = 1;
+  (*cuantos_primos)=0;
 
-	// Eligir o numero de processo que vamos usar
-	int n_processor = N_PROCESS_MAX;
-	if(entrada_size < N_PROCESS_MAX) n_processor = entrada_size;
-	
-	pid_t filhos[n_processor];
-	// creates pipe
-	int pipefd[2]; // pipe r=0/w=1
-	pipe(pipefd);
+  //definir o vetor de pid
+	pid_t filhos[N_PROCESS_MAX];
+	int id=0;
+	for(int k=0;k<entrada_size; k++){
 
-	// Escrever os int no pipe
-	for(i = 0; i < entrada_size; i++){
-		write(pipefd[1], &entrada[i], sizeof(entrada[i]));
+   if((*p_ativos)> N_PROCESS_MAX){ 
+      for(int w=k-3;w<k;w++){
+        waitpid(filhos[w], NULL, 0);
+        (*p_ativos)--;
+      }
+   }
+      filhos[k] = fork();
+      (*p_ativos)++;
+
+
+      if(filhos[k] == 0){
+          if(entrada[k]!=2 && entrada[k]!=3 && entrada[k]!=5 && entrada[k]!=7){
+            if(entrada[k]<=1);
+            else if(entrada[k]%2==0);
+            else if(entrada[k]%3==0);
+            else if(entrada[k]%5==0);
+            else if(entrada[k]%7==0);
+            else(*cuantos_primos)++;
+
+          }
+          else (*cuantos_primos)++;
+        exit(0);/*sair do processo*/
+      }
 	}
-	
-	//Fechar pipe write
-	close(pipefd[1]);
+  
+  // Todos os filhos foram gerados. Esperando...
+  for(int j = 0; j<i;j++){
+    waitpid(filhos[j], NULL, 0);
+  }
+  
+  // Escrever o numero de primos
+  printf("%d\n", *cuantos_primos);
 
-	// crear o numero de processes necessario
-	for(i = 0; i < n_processor; i++){
-		
-		filhos[i] = fork();
-
-		// Esta parte do codigo executa no processo filho
-		if(filhos[i] == 0){
-			long long int numero;
-			
-			// Fechar pipe write
-			close(pipefd[1]);
-
-			// reads pipe
-			while(read(pipefd[0], &numero, sizeof(numero)) > 0){
-
-				if(eh_primo(numero)) (*cuantos_primos)++;
-			}
-
-			// fechar pipe read
-			close(pipefd[0]);
-				
-			// Sair do processo
-			exit(0);
-		}
-	}
-
-	// Todos os filhos foram gerados. Esperando...
-	for(int k = 0; k < n_processor; k++){
-		waitpid(filhos[k], NULL, 0);
-	}
-	// Escrever o numero de primos
-	printf("%d\n", (*cuantos_primos));
-	return 0;
+  return 0;
 }
